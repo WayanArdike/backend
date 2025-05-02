c402b86fe42190a63f9a4753b7fe81bd70a1af8e
Wayan Ardike <113248931+WayanArdike@users.noreply.github.com>
2025-05-02 13:39:27 +0700

    Update index.py

--- index.py
+++ index.py
@@ -33,12 +33,10 @@ def predict():
 
     result = {
         'label': label,
-        'description': label.replace('_', ' ').title(),
         'probability': probability
     }
 
     return jsonify(result)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 3000)) 
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080)
